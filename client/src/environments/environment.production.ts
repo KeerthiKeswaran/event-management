000f0db73353cfd37198221e2df432bf19e95c8b
KeerthiKeswaran K S <keshwarankeerthi@gmail.com>
2026-07-13 21:33:51 +0530

    Fix API URL in env

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-api.azurecontainerapps.io/api',
-  serverUrl: 'https://backend-api.azurecontainerapps.io',
-  clientUrl: 'https://frontend-app.azurestaticapps.net'
+  apiUrl: 'https://ca-getmyevents-prod.greenmoss-13d328d8.southindia.azurecontainerapps.io/api',
+  serverUrl: 'https://ca-getmyevents-prod.greenmoss-13d328d8.southindia.azurecontainerapps.io',
+  clientUrl: 'https://brave-ocean-0c2962200.7.azurestaticapps.net'
 };
